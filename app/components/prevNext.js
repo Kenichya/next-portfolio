5424e2d47afc1bd543da58e625f1b3f67ef2d238
Ken-ichi Yasuda <msredcomet06@gmail.com>
2024-12-06 21:57:32 +0900

    prevNext bug fixed.
    A bug in prevNext is fixed.
    Arrow images links are added.

--- app/components/prevNext.js
+++ app/components/prevNext.js
@@ -9,13 +9,14 @@ const prevNext = (props) => {
     <div className="pnWrapper">
     { 0 < props.prev.length &&
       <Link href={`/blog/${props.prev[0].slug}`}>
-        <img src="/images/arow-left.svg" alt="arrow-left" />
         <h3>{props.prev[0].frontmatter.title}</h3>
+        <img src="/images/arrow-left.svg" alt="arrow-left" />
       </Link>
     }
     { 0 < props.prev.length &&
       <Link href={`/blog/${props.next[0].slug}`}>
         <h3>{props.next[0].frontmatter.title}</h3>
+        <img src="/images/arrow-right.svg" alt="arrow-right" />
       </Link>
     }
     </div>
