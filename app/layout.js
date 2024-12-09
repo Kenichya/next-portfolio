bc39b929c02368935f4fd0c1a90e92bf4cbb6277
Ken-ichi Yasuda <msredcomet06@gmail.com>
2024-12-09 11:16:21 +0900

    Styles are applied properly now.
    Styles on the top page are applied properly now.
    index.css is added in layout.js.

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import './styles/all.css'
 import './styles/blog.css'
 import './styles/common.css'
 import './styles/contact.css'
+import './styles/index.css'
 import './styles/singleBlog.css'
 
 import Header from './components/header'
